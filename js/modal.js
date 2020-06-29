a2b5a4c2083b7589bb9c98985905daca7e48a670
Denis Onyshchuk <o.82.denis@gmail.com>
2020-06-29 15:29:53 +0300

    make form-order-service : mobile

--- js/modal.js
+++ js/modal.js
@@ -10,7 +10,10 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open");
+
     refs.modal.classList.toggle("is-hidden");
+
     refs.blureWrapper.classList.toggle("blur");
   }
 })();
